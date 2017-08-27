81f1a6aa7ac81f6eeb9b047feae9deb0bff1e824
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2017-08-27 11:22:23 +0200

    Change default time

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
   def index
     @lanes = Lane.query(params[:date], params[:time], params[:lanes])
     @lane_count = params.has_key?(:lanes) ? params[:lanes].to_i : 1
-    @time = params.has_key?(:time) ? DateTime.parse(params[:time], "%H").to_time.strftime("%H") : Time.now.strftime("%H")
+    @time = params.has_key?(:time) ? DateTime.parse(params[:time], "%H").to_time.strftime("%H") : "17"
     @date = params.has_key?(:date) ? Date.strptime(params[:date], "%d-%m-%Y").strftime("%d-%m-%Y").to_s : Date.today.strftime("%d-%m-%Y").to_s
   end
 
