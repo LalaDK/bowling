c4292ebdc203995bdc1b5ad64e432dcd3d8718dd
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2017-08-27 11:20:18 +0200

    Fixes again

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,13 +2,13 @@ class MainController < ApplicationController
 
   def index
     @lanes = Lane.query(params[:date], params[:time], params[:lanes])
-    @lane_count = params.has_key?(:lanes) ? params[:lanes].to_i : 1 
+    @lane_count = params.has_key?(:lanes) ? params[:lanes].to_i : 1
     @time = params.has_key?(:time) ? DateTime.parse(params[:time], "%H").to_time.strftime("%H") : Time.now.strftime("%H")
     @date = params.has_key?(:date) ? Date.strptime(params[:date], "%d-%m-%Y").strftime("%d-%m-%Y").to_s : Date.today.strftime("%d-%m-%Y").to_s
   end
 
   def create
     Lane.update(params[:date], params[:time], params[:lanes])
-    redirect_to action: :index, params: {date: params[:date], time: params[:time], lanes: params[:lanes]}
+    redirect_to root: :index, params: {date: params[:date], time: params[:time], lanes: params[:lanes]}
   end
 end
